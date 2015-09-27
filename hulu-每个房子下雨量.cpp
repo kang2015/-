945cc5b474031f0305d592d60b374179083ead87
kang2015 <kangyanli001@163.com>
2015-09-27 23:15:08 +0800

    Update hulu-每个房子下雨量.cpp

--- hulu-每个房子下雨量.cpp
+++ hulu-每个房子下雨量.cpp
@@ -52,6 +52,136 @@ int main(){
 
 //end of my ans
 
+//my ans of "线段树"方法
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include<memory>
+#include<map>
+#include<queue>
+#include <sstream>
+#include <set>
+#include <list>
+using namespace std;
+const int INFINITE = INT_MAX;
+const int MAXNUM = 1000;
+struct SegTreeNode
+{
+    int beginPos;
+    int endPos;
+    int addMark;//延迟标记
+}segTree[MAXNUM];//定义线段树
+
+/*
+  功能：构建线段树
+  root：当前线段树的根节点下标
+  arr: 用来构造线段树的数组
+  istart：数组的起始位置
+  iend：数组的结束位置
+  */
+void build(int root,vector<int> &arr,int istart, int iend)
+{
+    segTree[root].addMark = 0;//----设置标延迟记域
+    if(istart == iend){//叶子节点
+        segTree[root].beginPos = arr[istart];
+        segTree[root].endPos = arr[iend];
+    }else
+        {
+            segTree[root].beginPos = arr[istart];
+            segTree[root].endPos = arr[iend];
+            int mid = (istart + iend) / 2;
+            build(root*2+1, arr, istart, mid);//递归构造左子树
+            build(root*2+2, arr, mid+1, iend);//递归构造右子树
+            
+        }
+}
+
+/*
+  功能：当前节点的标志域向孩子节点传递
+  root: 当前线段树的根节点下标
+  */
+void pushDown(int root)
+{
+    if(segTree[root].addMark != 0)
+        {
+            //设置左右孩子节点的标志域，因为孩子节点可能被多次延迟标记又没有向下传递
+            //所以是 “+=”
+            segTree[root*2+1].addMark += segTree[root].addMark;
+            segTree[root*2+2].addMark += segTree[root].addMark;
+            //传递后，当前节点标记域清空
+            segTree[root].addMark = 0;
+        }
+}
+
+/*
+  功能：线段树的区间查询
+  root：当前线段树的根节点下标
+  [nstart, nend]: 当前节点所表示的区间
+  [qstart, qend]: 此次查询的区间
+  */
+int query(int root, int qstart, int qend)
+{
+    //查询区间和当前节点区间没有交集
+    if(qstart > segTree[root].endPos || qend < segTree[root].beginPos)
+        return INFINITE;
+    //当前节点区间包含在查询区间内
+    if(qstart <= segTree[root].beginPos && qend >= segTree[root].endPos)
+        return segTree[root].addMark;
+    //分别从左右子树查询，返回两者查询结果的较小值
+    pushDown(root); //----延迟标志域向下传递
+    return min(query(root*2+1,qstart, qend),
+                query(root*2+2, qstart, qend));
+    
+}
+
+/*
+  功能：更新线段树中某个区间内叶子节点的值
+  root：当前线段树的根节点下标
+  [nstart, nend]: 当前节点所表示的区间
+  [ustart, uend]: 待更新的区间
+  addVal: 更新的值（原来的值加上addVal）
+  */
+void update(int root,int ustart, int uend, int addVal)
+{
+    //更新区间和当前节点区间没有交集
+    if(ustart > segTree[root].endPos || uend < segTree[root].beginPos)
+        return ;
+    //当前节点区间包含在更新区间内
+    if(ustart <= segTree[root].beginPos && uend >= segTree[root].endPos){
+            segTree[root].addMark += addVal;
+            //segTree[root].val += addVal;
+            return ;
+    }
+    pushDown(root); //延迟标记向下传递
+    //更新左右孩子节点
+    update(root*2+1,  ustart, uend, addVal);
+    update(root*2+2,  ustart, uend, addVal);
+    //根据左右子树的值回溯更新当前节点的值
+    //segTree[root].val = min(segTree[root*2+1].val, segTree[root*2+2].val);
+}
+int main(){
+    int n,m;
+    cin >> n;
+    vector<int> arr(n,-1);
+    for(int i=0;i<n;i++)
+        cin >> arr[i];
+    build(0, arr, 0, n-1);
+    int left,right,w;
+    cin >> m;
+    for(int i=0;i<m;i++){
+        cin >> left >> right >> w;
+        update(0, left, right, w);
+    }
+    for(int i=0;i<n;i++){
+        cout << query(0, arr[i], arr[i])<<endl;
+    }
+    return 0;
+}
+//end of my ans of "线段树"方法
+
 //yz li's ans java
 package com.li.test;
 
